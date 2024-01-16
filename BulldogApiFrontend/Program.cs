4b3166539d1b7abeb380b72018346d3b645f5868
MQ29 <matiax2829@gmail.com>
2024-01-16 20:11:39 +0100

    Modal with employees

--- BulldogApiFrontend/Program.cs
+++ BulldogApiFrontend/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Modal;
 using BulldogApiFrontend.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -13,6 +14,7 @@ namespace BulldogApiFrontend
             builder.RootComponents.Add<HeadOutlet>("head::after");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7112") });
             builder.Services.AddScoped<IServiceApiService, ServiceApiService>();
+            builder.Services.AddBlazoredModal();
             await builder.Build().RunAsync();
         }
     }
