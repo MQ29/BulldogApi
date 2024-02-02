6b9f0c354423256610033b3075e6c70eef2dd0fc
MQ29 <matiax2829@gmail.com>
2024-02-02 21:51:04 +0100

    ownerpath

---
+++ BulldogApiFrontend/Models/CustomAuthProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BulldogApiFrontend.Models
+{
+    public class CustomAuthProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BulldogApiFrontend/Services/EmailService.cs
+++ BulldogApiFrontend/Services/EmailService.cs
@@ -3,5 +3,7 @@
     public class EmailService
     {
         public string Email { get; set; }
+        public bool isNew { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
